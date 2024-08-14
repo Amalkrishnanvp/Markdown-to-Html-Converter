8a5e44f065c23de4a76936b17df70414bc5c1090
Amalkrishnanvp <Amalyedhu10@gmail.com>
2024-08-14 16:28:55 +0530

    Finished designing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Markdown - Html" });
+  res.render("index", {
+    title: "Markdown - Html",
+    heading: "Markdown to HTML Converter",
+  });
 });
 
 module.exports = router;
